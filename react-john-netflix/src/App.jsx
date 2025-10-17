6c60fb9c69fe771aab5fb5abb57c8c12798752ee
여인영 <marineyeong@gmail.com>
2025-10-17 14:47:26 +0900

    이미지 배너 생성 및 텍스트 100글자 이상일 경우 줄임 설정

--- react-john-netflix/src/App.jsx
+++ react-john-netflix/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './components/Banner.jsx';
 import Nav from './components/Nav.jsx';
 
 function App() {
   return (
     <>
       <Nav />
+      <Banner />
     </>
   );
 }
