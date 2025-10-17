2938ce0858cc04b84527d9f442b065147e08c964
여인영 <marineyeong@gmail.com>
2025-10-17 14:01:20 +0900

    스크롤 시 nav 백그라운드 컬러 변경 효과

--- react-john-netflix/src/components/Nav.jsx
+++ react-john-netflix/src/components/Nav.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import './Nav.css';
 
 const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
   return (
-    <nav className="nav">
+    <nav className={`nav ${show && 'nav__black'}`}>
       <img
         alt="Netflix log"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
