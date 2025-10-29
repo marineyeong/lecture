c3a223847fc95bec744f65a4fda45de131cda0de
여인영 <marineyeong@gmail.com>
2025-10-29 17:43:51 +0900

    styled components를 활용하여 footer 생성

--- react-john-netflix/src/App.jsx
+++ react-john-netflix/src/App.jsx
@@ -1,6 +1,7 @@
 import requests from './api/requests.js';
 import './App.css';
 import Banner from './components/Banner.jsx';
+import Footer from './components/Footer.jsx';
 import Nav from './components/Nav.jsx';
 import Row from './components/Row.jsx';
 
@@ -14,6 +15,7 @@ function App() {
       <Row title="Top Rated" id="TR" fetchURL={requests.fetchTopRated} />
       <Row title="Action Movies" id="AM" fetchURL={requests.fetchActionMovies} />
       <Row title="Comedy Movies" id="CM" fetchURL={requests.fetchComedyMovies} />
+      <Footer />
     </>
   );
 }
